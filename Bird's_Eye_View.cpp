fb6d9a448bd804e43a8d240db688a5dcb9627866
Nowely <Nowely@github.com>
2019-05-21 00:11:55 +0300

    New version
    Add ROI, new window with source video, grid rotatopn

--- Bird's_Eye_View.cpp
+++ Bird's_Eye_View.cpp
@@ -5,41 +5,53 @@
 #include <string>
 #include "opencv2/opencv_modules.hpp"
 
+#include "opencv2/video/background_segm.hpp"
+#include "opencv2/video/tracking.hpp"
+#include <Windows.h>
 
 using namespace std;
 using namespace cv;
 #define PI 3.1415926
 
 
-int frameWidth = 960;
-int frameHeight = 720;
+int frameWidth = 800;
+int frameHeight = 450;
 
 int main() {
 	  
-	bool playVideo = true;
-    string filename = "video3.mp4"; // Имя файла тут
+    string filename = "video4.mp4"; // Имя файла тут
     VideoCapture capture(filename);
   
-    Mat source, destination;
+    Mat source, source1, destination;
 
 	int alpha_ = 90, beta_ = 90, gamma_ = 90; // Инициализация параметров
 	int f_ = 500, dist_ = 500;
 	int pxstep_ = 75;
+	int image_h = 223;
+	int image_w = 900;
+	int turn_ = 500;
+	Rect Rec(1080 - image_w, 720 - image_h, image_w, image_h);
 
-	namedWindow("Result", 1); 
+	namedWindow("Original", 1);
+	namedWindow("Result", 1);
 	createTrackbar("Alpha", "Result", &alpha_, 180);
 	createTrackbar("Beta", "Result", &beta_, 180);
 	createTrackbar("Gamma", "Result", &gamma_, 180);
 	createTrackbar("Фокус", "Result", &f_, 2000);
 	createTrackbar("Дистанция", "Result", &dist_, 2000);
 	createTrackbar("Шаг Сетки", "Result", &pxstep_, 150);
+	createTrackbar("Поворот", "Result", &turn_, 1000);
+
 
 	while( true ) {
 		
-		capture >> source;		
-
+		capture >> source1;		
+		rectangle(source1, Rec, Scalar(255), 1, 8, 0); //ROI
+		source = source1(Rec);
+		imshow("Original", source1);
 
 		resize(source, source,Size(frameWidth, frameHeight));
+
 
 		double focalLength, dist, alpha, beta, gamma; 
 
@@ -101,26 +113,25 @@ int main() {
 
 		int x = 0;
 		int	y = 0;
-		while (x < destination.cols)
+		while (x < destination.cols + 3000)
 		{
-			line(destination, Point(x, 0), Point(x, destination.rows), Scalar(0, 255, 0), 1);
+			line(destination, Point(x - 1000, 0), Point(x - 1000 + turn_ - 500, destination.rows), Scalar(0, 255, 0), 1);
 			x += pxstep_;
 		}
-		while (y < destination.rows)
+		while (y < destination.rows + 3000)
 		{
-			line(destination, Point(0, y), Point(destination.cols, y), Scalar(0, 255, 0), 1);
+			line(destination, Point(0, y - 1000), Point(destination.cols, y - 1000 + turn_ - 500), Scalar(0, 255, 0), 1);
 			y += pxstep_;
 		}
 
 		imshow("Result", destination);
+		
 
-		
-		
 		if (cv::waitKey(5) == 'p')
 			while (cv::waitKey(5) != 'p');
-		if (cv::waitKey(5) == 'з')
-			while (cv::waitKey(5) != 'p');
-		/*waitKey(100);*/
+		
+
+		//waitKey(100);
 	}
 
 
